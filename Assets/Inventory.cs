c6c4697f9410b159ac90abf30228b7b834d72471
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-23 09:30:56 +1100

    boom

---
+++ Assets/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool _showInv;
+    private Vector2 scr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            _showInv = !_showInv;
+            if (_showInv)
+            {
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                return;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                return;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (_showInv)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width / 3, Screen.height), "Inventory");
+
+            scr.x = Screen.width / 3;
+            scr.y = Screen.height;
+            GUI.Box(new Rect(scr.x / 2f, scr.y, 1f, 1f),"Boom");
+            Layout();
+        }
+    }
+
+    void Layout()
+    {
+    }
+}
