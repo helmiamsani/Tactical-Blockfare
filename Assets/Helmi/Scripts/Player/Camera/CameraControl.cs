8168c991e508dbadeceda22c2a82597f511e8b05
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-24 21:30:02 +1100

    Improvisation

---
+++ Assets/Helmi/Scripts/Player/Camera/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float turnSpeed = 1.5f;
+    public float turnSmooth = .1f;
+    public float tiltMax = 75f;
+    public float tiltMin = 45f;
+
+    private float _lookAngle;
+    private float _tiltAngle;
+
+    private float _smoothX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Helmi/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float mouseX;
+    public float mouseY;
+    public float moveToward;
+    public float moveBack;
+    public bool fire1;
+    public bool jump;
+    public bool crouch;
+    public bool getDown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X"); // Input for moving mouse on the X axis.
+        mouseY = Input.GetAxis("Mouse Y"); // Input for moving mouse on the Y axis.
+        moveToward = Input.GetAxis("Horizontal"); // Input for moving player toward.
+        moveBack = Input.GetAxis("Vertical"); // Input for moving player backward.
+        fire1 = Input.GetMouseButtonDown(0); // Input for firing.
+        jump = Input.GetKeyDown(KeyCode.Space); // Input for jumping.
+        crouch = Input.GetKeyDown(KeyCode.C); // Input for crouch.
+        getDown = Input.GetKeyDown(KeyCode.LeftControl); // Input for getdown.
+    }
+}
