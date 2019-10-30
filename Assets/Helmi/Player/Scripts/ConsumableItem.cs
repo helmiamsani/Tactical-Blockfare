7166da3001b5aa1e4f83b614b311a63959aa3a50
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-30 11:59:13 +1100

    boom

---
+++ Assets/Helmi/Player/Scripts/ConsumableItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumableItem : MonoBehaviour
+{
+    private PlayerInput _plyrInput;
+    private Camera _mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _plyrInput
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Helmi/Player/Scripts/PlayerInput.cs
+++ Assets/Helmi/Player/Scripts/PlayerInput.cs
@@ -14,6 +14,7 @@ public class PlayerInput : MonoBehaviour
     public bool jump;
     public bool crouch;
     public bool getDown;
+    public bool interact;
 
     // Update is called once per frame
     void Update()
@@ -28,5 +29,6 @@ public class PlayerInput : MonoBehaviour
         jump = Input.GetKeyDown(KeyCode.Space); // Input for jumping.
         crouch = Input.GetKeyDown(KeyCode.C); // Input for crouch.
         getDown = Input.GetKeyDown(KeyCode.LeftControl); // Input for getdown.
+        interact = Input.GetKey(KeyCode.E); // Input for interact.
     }
 }
