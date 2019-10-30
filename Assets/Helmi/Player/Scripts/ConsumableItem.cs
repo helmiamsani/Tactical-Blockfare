465070305b04883e7a2d5f71545ba2cb96ce7b92
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-30 15:17:52 +1100

    boom

--- Assets/Helmi/Player/Scripts/ConsumableItem.cs
+++ Assets/Helmi/Player/Scripts/ConsumableItem.cs
@@ -6,16 +6,30 @@ public class ConsumableItem : MonoBehaviour
 {
     private PlayerInput _plyrInput;
     private Camera _mainCam;
+    private Transform _rayOut;
 
     // Start is called before the first frame update
     void Start()
     {
-        _plyrInput
+        _plyrInput = GetComponent<PlayerInput>();
+        _mainCam = GetComponentInChildren<Camera>();
+        _rayOut = _mainCam.GetComponentInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_plyrInput.interact)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(_rayOut.position, Vector3.forward, out hit, 10))
+            {
+                if (hit.collider.CompareTag("Item"))
+                {
+                    Debug.Log("It's an item");
+                }
+            }
+        }
     }
 }
